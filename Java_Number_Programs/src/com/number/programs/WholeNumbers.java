cde93baf4186a14663de1eec09d14f2d2315163b
KommadiBasha <kommadibasha2017@gmail.com>
2024-08-28 11:07:11 +0530

    Number Programs commit 1

---
+++ Java_Number_Programs/src/com/number/programs/WholeNumbers.java
@@ -0,0 +1,17 @@
+package com.number.programs;
+
+import java.util.Scanner;
+
+public class WholeNumbers {
+
+	public static void main(String[] args) {
+		
+		int number=0;
+		while(number<=50)
+		{
+			System.out.print(number +" ");
+			number++;
+		}
+	}
+
+}
